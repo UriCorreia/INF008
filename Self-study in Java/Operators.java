4ceff63295dd3004891857f0e73f4e6c37d655f0
Uriel Correia <uriel2405correia@gmail.com>
2025-10-09 19:09:52 +0000

    Operators

---
+++ Self-study in Java/Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        
+        int var = 1 + 2;
+        // System.out.println(var);
+
+        // var = var * 2;
+        // System.out.println(var);
+
+        // var = var - 2;
+        // System.out.println(var);
+
+        // var = var % 2;
+        // System.out.println(var);
+
+        // String firstName = "This is ";
+        // String secondName = "a concatenated String.";
+        // String thirdName = firstName + secondName;
+        // System.out.println(thirdName);
+
+        System.out.println(var++); // adiciona o valor depois de imprimir na tela;
+        System.out.println(++var); //  adiciona o valor antes de imprimir na tela;
+    }
+}
---
+++ Self-study in Java/OperatorsRel.java
@@ -0,0 +1,16 @@
+public class OperatorsRel {
+
+    public static void main(String[] args) {
+
+        int valor_1 = 1;
+        int valor_2 = 2;
+
+
+        // Utilizando da seguinte forma, vai ser retornado um booleano
+        System.out.println("valor_1 == valor_2: " + (valor_1 == valor_2));
+        System.out.println("valor_1 != valor_2: " + (valor_1 != valor_2));
+        System.out.println("valor_1 > valor_2: " + (valor_1 > valor_2));
+        System.out.println("valor_1 < valor_2: " + (valor_1 < valor_2));
+
+    }
+}
